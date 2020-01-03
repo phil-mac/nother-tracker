753fff2d8963fa8f9be621d6b9bd14bed1a56740
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2020-01-02 18:41:49 -0800

    added colors

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -3,8 +3,17 @@ import '../App.css';
 import Fab from '@material-ui/core/Fab';
 
 const colors = [
-    'lightblue',
-    'mediumpurple'
+    '#EDF2F7',
+    '#FED7D7',
+    '#FEEBC8',
+    '#FEFCBF',
+    '#C6F6D5',
+    '#B2F5EA',
+    '#BEE3F8',
+    '#C3DAFE',
+    '#E9D8FD',
+    '#FED7E2',
+
 ]
 
 export default (props) => {
